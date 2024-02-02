7e789372d3b0945858fa3ac88a351fbc13282171
Praveen <praveenrajesh2001@gmail.com>
2024-02-02 23:24:14 +0530

    commit

--- components/About.jsx
+++ components/About.jsx
@@ -44,14 +44,19 @@ const qualificationData = [
         title: 'education',
         data: [
             {
-                university: 'Thiagarajar College',
+                university: 'Kongu Engineering College, Erode',
+                qualification: 'Master of Computer Applications',
+                years: '2022 - Present',
+            },
+            {
+                university: 'Thiagarajar College, Madurai',
                 qualification: 'Bachelor of Commerce (Computer Applications)',
                 years: '2019 - 2022',
             },
             {
-                university: 'Kongu Engineering College',
-                qualification: 'Master of Computer Applications',
-                years: '2022 - 2024',
+                university: 'St. Francis PU College, Bengaluru',
+                qualification: 'Commerce',
+                years: '2018 - 2019',
             },
         ],
     },
@@ -74,7 +79,7 @@ const skillData = [
                 name: 'ReactJS',
             },
             {
-                name: 'Front-end Development',
+                name: 'MongoDB',
             },
         ],
     },
@@ -91,7 +96,7 @@ const skillData = [
                 imgPath: '/about/pec.svg',
             },
             {
-                imgPath: '/about/pof.svg',
+                imgPath: '/about/pGit.svg',
             },
         ],
     },
@@ -135,7 +140,7 @@ const About = () => {
                                         MCA Student with keen interest in Technology.
                                     </h3>
                                     <p className='subtitle max-w-xl mx-auto xl:mx-0'>
-                                    Extremely motivated MCA student that has a desire to succeed in the profession and has a strong passion for technology. 
+                                    Extremely motivated student having a desire to succeed in the profession and a strong passion for technology. 
                                     </p>
                                     {/* icons */}
                                     <div className='grid xl:grid-cols-2 gap-4 mb-12'>
@@ -159,8 +164,104 @@ const About = () => {
                                     </div>
                                 </div>
                             </TabsContent>
-                            <TabsContent value='qualification'>quali</TabsContent>
-                            <TabsContent value='skills'>skills</TabsContent>
+                            {/* qualification */}
+                            <TabsContent value='qualification'>
+                                <div>
+                                    <h3 className="h3 mb-8 text-center xl:text-left">
+                                        My Journey
+                                    </h3>
+                                    {/* experience and education wrapper */}
+                                    <div className="">
+                                    {/* experience(later) */}
+                                    {/* education */}
+                                        <div className="flex flex-col gap-y-6">
+                                            <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                                                <GraduationCap size={28} />
+                                                <h4 className="capitalize font-medium">
+                                                    {getData(qualificationData, 'education').title}
+                                                </h4>
+                                            </div>
+                                            {/* list */}
+                                            <div className="flex flex-col gap-y-8">
+                                                {getData(qualificationData, 'education').data.map(
+                                                    (item, index) => {
+                                                        const { university, qualification, years } = item;
+                                                        return(
+                                                            <div className="flex gap-x-8 group" key={index}>
+                                                                <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                    <div className='w-[11px] h-[11px] rounded-full
+                                                                    bg-primary absolute -left-[5px]
+                                                                    group-hover:translate-y-[84px] transition-all
+                                                                    duration-500'></div>
+                                                                </div>
+                                                                <div>
+                                                                    <div className='font-semibold text-xl leading-none
+                                                                    mb-2'>
+                                                                        {university}
+                                                                    </div>
+                                                                    <div className='text-lg leading-none text-muted-foreground
+                                                                    mb-3'>
+                                                                        {qualification}
+                                                                    </div>
+                                                                    <div className="text-base font-medium">
+                                                                        {years}
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        );
+                                                    }
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </TabsContent>
+                            {/* skills */}
+                            <TabsContent value='skills'>
+                                <div className='text-center xl:text-left'>
+                                    <h3 className="h3 mb-8">Skills and Tools</h3>
+                                    {/* skills */}
+                                    <div className="mb-16">
+                                        <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                                        <div className="border-b border-border mb-4"></div>
+                                        {/* skill list */}
+                                        <div>
+                                            {getData(skillData, 'skills').data.map(
+                                                (item, index) => {
+                                                    const { name } = item;
+                                                    return (
+                                                        <div className="mb-1 w-2/4 text-center xl:text-left mx-auto 
+                                                        xl:mx-0"
+                                                        key={index}
+                                                        >
+                                                            <div className="font-medium">{name}</div>
+                                                        </div>
+                                                    );
+                                                }
+                                            )}
+                                        </div>
+                                    </div>
+                                    {/* tools */}
+                                    <div>
+                                        <h4 className='text-xl font-semibold mb-2 xl:text-left'>
+                                            Tools
+                                        </h4>
+                                        <div className='border-b border-border mb-5'></div>
+                                        {/* tools list */}
+                                        <div className='flex gap-x-8 justify-center xl:justify-start'>
+                                            {getData(skillData, 'tools').data.map((item, index) => {
+                                                const {imgPath} = item;
+                                                return(
+                                                    <div key={index}>
+                                                        <Image src={imgPath} width={48} height={48} alt=''
+                                                        priority />
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+                                    </div>
+                                </div>
+                            </TabsContent>
                         </div>
                     </Tabs>
                 </div>
